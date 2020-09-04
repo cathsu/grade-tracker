c72e61868df38938af237951048be65447bae7c4
Cathy Hsu <chsucathy@gmail.com>
2020-09-03 19:24:50 -0700

    Add GradeTrackerDatabase

---
+++ app/src/main/java/com/example/gradetracker/DB/GradeTrackerDatabase.java
@@ -0,0 +1,34 @@
+package com.example.gradetracker.DB;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.gradetracker.Model.Assignment;
+import com.example.gradetracker.Model.Course;
+import com.example.gradetracker.Model.User;
+
+@Database(entities = {Assignment.class, Course.class, User.class}, version = 1)
+public abstract class GradeTrackerDatabase extends RoomDatabase {
+
+    private static GradeTrackerDatabase instance;
+
+    // Todo: Uncomment when AssignmentDao and CourseDao are merged in
+//    public abstract AssignmentDao AssignmentDao();
+//
+//    public abstract CourseDao CourseDao();
+
+    public abstract UserDao UserDao();
+
+    public static synchronized GradeTrackerDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    GradeTrackerDatabase.class, "grade_tracker_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
