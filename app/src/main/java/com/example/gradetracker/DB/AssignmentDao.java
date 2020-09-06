8037e0459e98feedac9df2de1c1efee7ae4e84c1
Kevin <ktpiffero@aol.com>
2020-09-06 15:13:31 -0700

    Updated Assignment & Course Dao

--- app/src/main/java/com/example/gradetracker/DB/AssignmentDao.java
+++ app/src/main/java/com/example/gradetracker/DB/AssignmentDao.java
@@ -1,5 +1,47 @@
 package com.example.gradetracker.DB;
 
-//comment
+
+import com.example.gradetracker.Model.Assignment;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+
+
+@Dao
 public interface AssignmentDao {
+
+    @Insert
+    void insert(Assignment... assignment);
+
+    @Update
+    void update(Assignment...assignment);
+
+    @Delete
+    void delete(Assignment assignment);
+
+    @Query("SELECT * FROM assignment WHERE assignment_description = :assignDescription")
+    Assignment getAssignmentId(String assignDescription);
+
+    @Query("SELECT * FROM assignment WHERE max_points = :maxPoints")
+    List<Assignment> getAssignmentMaxPoints(String maxPoints);
+
+    @Query("SELECT * FROM assignment WHERE earned_points = :earnPoints")
+    List<Assignment> getAssignmentEarnPoints(int earnPoints);
+
+    @Query("SELECT * FROM assignment WHERE assigned_date = :assignedDate")
+    List<Assignment> getAssignmentAssignedDate(String assignedDate);
+
+    @Query("SELECT * FROM assignment WHERE due_date = :dueDate")
+    List<Assignment> getAssignmentDueDate(String dueDate);
+
+    @Query("SELECT * FROM assignment WHERE category_name = :categoryName")
+    List<Assignment> getAssignmentCategoryName(String categoryName);
+
+    @Query("SELECT * FROM assignment WHERE course_id = :courseId")
+    List<Assignment> getAssignmentInCourseId(int courseId);
 }
+
