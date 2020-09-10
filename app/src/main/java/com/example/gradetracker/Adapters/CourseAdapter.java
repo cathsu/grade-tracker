ab3528fb60b10575c6480a5b19b3eba926d6960e
Denize Ignacio <denizeignacio@gmail.com>
2020-09-10 16:01:20 -0700

    Created recylerview for courses list

---
+++ app/src/main/java/com/example/gradetracker/Adapters/CourseAdapter.java
@@ -0,0 +1,66 @@
+package com.example.gradetracker.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.gradetracker.Model.Course;
+import com.example.gradetracker.R;
+import com.example.gradetracker.databinding.ItemCourseBinding;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.ViewHolder> {
+    private List<Course> mCourses;
+    private ItemCourseBinding itemCourseBinding;
+
+    public CourseAdapter(List<Course> courses) {
+        mCourses = courses;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View courseView = inflater.inflate(R.layout.item_course, parent, false);
+        itemCourseBinding = ItemCourseBinding.inflate(inflater);
+        return new ViewHolder(courseView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseAdapter.ViewHolder holder, int position) {
+        Course currentCourse = mCourses.get(position);
+        holder.bind(currentCourse);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourses.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        // TODO add relevant view texts here
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), "Working", Toast.LENGTH_LONG).show();
+        }
+
+        public void bind(Course currentCourse) {
+            itemCourseBinding.tvCourseName.setText(currentCourse.getCourseName());
+            itemCourseBinding.tvInstructorName.setText(currentCourse.getInstructor());
+            itemCourseBinding.tvStartDate.setText(currentCourse.getStartDate());
+            itemCourseBinding.tvEndDate.setText(currentCourse.getEndDate());
+            itemCourseBinding.tvDescription.setText(currentCourse.getDescription());
+        }
+    }
+}
--- app/src/main/java/com/example/gradetracker/CoursesActivity.java
+++ app/src/main/java/com/example/gradetracker/CoursesActivity.java
@@ -1,14 +1,37 @@
 package com.example.gradetracker;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.gradetracker.Adapters.CourseAdapter;
+import com.example.gradetracker.DB.AppDatabase;
+import com.example.gradetracker.Model.Course;
+import com.example.gradetracker.Model.User;
+import com.example.gradetracker.databinding.ActivityCoursesBinding;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CoursesActivity extends AppCompatActivity {
+    private ActivityCoursesBinding activityCoursesBinding;
+    private AppDatabase db;
+    ArrayList<Course> courses = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_courses);
+        db = AppDatabase.getInstance(getApplicationContext());
+        activityCoursesBinding = ActivityCoursesBinding.inflate(getLayoutInflater());
+        View view = activityCoursesBinding.getRoot();
+        setContentView(view);
+        String username = getIntent().getStringExtra("username");
+        User user = db.userDao().getUserWithUsername(username);
+        courses = user.getCourses();
+
+        // recycler view logic
+        CourseAdapter adapter = new CourseAdapter(courses);
+        activityCoursesBinding.rvCourses.setAdapter(adapter);
+        activityCoursesBinding.rvCourses
     }
 }
