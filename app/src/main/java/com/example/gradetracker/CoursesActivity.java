6c399c6c45a5f75f1f84522b109333710045a854
Denize Ignacio <41974125+dignacio0815@users.noreply.github.com>
2020-09-10 22:10:43 -0700

    Created recylerview for courses list (#7)
    * Created recylerview for courses list
    
    * added courses recycler view
    
    * Removed comments

--- app/src/main/java/com/example/gradetracker/CoursesActivity.java
+++ app/src/main/java/com/example/gradetracker/CoursesActivity.java
@@ -1,14 +1,44 @@
 package com.example.gradetracker;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.gradetracker.Adapters.CourseAdapter;
+import com.example.gradetracker.DB.AppDatabase;
+import com.example.gradetracker.Model.Course;
+import com.example.gradetracker.Model.User;
+import com.example.gradetracker.databinding.ActivityCoursesBinding;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 public class CoursesActivity extends AppCompatActivity {
+    private ActivityCoursesBinding activityCoursesBinding;
+    private AppDatabase db;
+    ArrayList<Course> courses = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_courses);
+        db = AppDatabase.getInstance(getApplicationContext());
+        activityCoursesBinding = ActivityCoursesBinding.inflate(getLayoutInflater());
+        View view = activityCoursesBinding.getRoot();
+        setContentView(view);
+        String username = getIntent().getStringExtra("username");
+        User user = db.userDao().getUserWithUsername(username);
+        courses = user.getCourses();
+        if (courses.isEmpty()) {
+            Toast toast = Toast.makeText(getApplicationContext(),"No courses to display. Feel free to add some!", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        } else {
+            CourseAdapter adapter = new CourseAdapter(courses);
+            activityCoursesBinding.rvCourses.setAdapter(adapter);
+            activityCoursesBinding.rvCourses.setLayoutManager(new LinearLayoutManager(this));
+        }
     }
 }
