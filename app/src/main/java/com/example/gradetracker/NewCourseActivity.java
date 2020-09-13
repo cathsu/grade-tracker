428a527e202cc6b2970d58bc9825959ccdf855e0
Denize Ignacio <41974125+dignacio0815@users.noreply.github.com>
2020-09-12 19:33:29 -0700

    Trying out extended FAB button (#10)
    * Trying out extended FAB button
    
    * UI design for adding a new course

--- app/src/main/java/com/example/gradetracker/NewCourseActivity.java
+++ app/src/main/java/com/example/gradetracker/NewCourseActivity.java
@@ -1,14 +1,26 @@
 package com.example.gradetracker;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.gradetracker.databinding.ActivityNewCourseBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewCourseActivity extends AppCompatActivity {
+    private ActivityNewCourseBinding activityNewCourseBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_course);
+        activityNewCourseBinding = ActivityNewCourseBinding.inflate(getLayoutInflater());
+        View view = activityNewCourseBinding.getRoot();
+        setContentView(view);
+//        activityNewCourseBinding.button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(NewCourseActivity.this, activityNewCourseBinding.tietCourseName.getText(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
