be31d123b993a00a3b79970c48b2b7e52bdf9654
Cathy Hsu <chsucathy@gmail.com>
2020-09-16 18:01:28 -0700

    Merge with master

---
+++ app/src/main/java/com/example/gradetracker/EditCourseActivity.java
@@ -0,0 +1,23 @@
+package com.example.gradetracker;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.gradetracker.DB.AppDatabase;
+import com.example.gradetracker.databinding.ActivityEditCourseBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditCourseActivity extends AppCompatActivity {
+    private ActivityEditCourseBinding activityEditCourseBinding;
+    private AppDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityEditCourseBinding = ActivityEditCourseBinding.inflate(getLayoutInflater());
+        View view = activityEditCourseBinding.getRoot();
+        setContentView(view);
+        db = AppDatabase.getInstance(getApplicationContext());
+    }
+}
