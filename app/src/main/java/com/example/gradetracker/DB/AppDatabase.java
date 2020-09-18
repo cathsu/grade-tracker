fdae88bd6f778a17835022f6b8c30fad68924c89
Cathy Hsu <chsucathy@gmail.com>
2020-09-18 13:32:55 -0700

    Remove comments

--- app/src/main/java/com/example/gradetracker/DB/AppDatabase.java
+++ app/src/main/java/com/example/gradetracker/DB/AppDatabase.java
@@ -2,15 +2,15 @@ package com.example.gradetracker.DB;
 
 import android.content.Context;
 
-import com.example.gradetracker.DB.TypeConverters.ArrayListConverter;
-import com.example.gradetracker.Model.Assignment;
-import com.example.gradetracker.Model.Course;
-import com.example.gradetracker.Model.User;
-
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+
+import com.example.gradetracker.DB.TypeConverters.ArrayListConverter;
+import com.example.gradetracker.Model.Assignment;
+import com.example.gradetracker.Model.Course;
+import com.example.gradetracker.Model.User;
 
 @Database(entities = {Assignment.class, Course.class, User.class}, version = 12)
 @TypeConverters({ArrayListConverter.class})
@@ -18,9 +18,8 @@ public abstract class AppDatabase extends RoomDatabase {
     public static final String DBNAME ="grade_tracker_db";
     private static AppDatabase instance;
 
-    // Todo: Uncomment when AssignmentDao and CourseDao are merged in
     public abstract AssignmentDao AssignmentDao();
-//
+
     public abstract CourseDao courseDao();
 
     public abstract UserDao userDao();
