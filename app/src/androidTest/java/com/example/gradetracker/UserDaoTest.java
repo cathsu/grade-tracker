98c1876537c3a8bb491a813151857f96509486b4
Cathy Hsu <chsucathy@gmail.com>
2020-09-17 23:54:42 -0700

    Add javadocs to UserDao

---
+++ app/src/androidTest/java/com/example/gradetracker/UserDaoTest.java
@@ -0,0 +1,132 @@
+package com.example.gradetracker;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.example.gradetracker.DB.AppDatabase;
+import com.example.gradetracker.Model.Assignment;
+import com.example.gradetracker.Model.Course;
+import com.example.gradetracker.Model.User;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class UserDaoTest {
+    private AppDatabase db;
+
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
+
+    @Test
+    public void testInsertUser throws Exception {
+        // insert user into database
+        User user = new User("username", "password123", "Tony", "Stark", null);
+        db.userDao().insertUser(user);
+
+        // retrieve assignment from database
+        Assignment dbAssignment = db.AssignmentDao().getUser
+
+        // ensure the retrieved assignment has valid attributes
+        assertEquals(dbAssignment.getName(), "Exam 1");
+        assertEquals(dbAssignment.getAssignmentDescription(), "This is the first exam");
+        assertEquals(dbAssignment.getMaxPoints(), new Integer(100));
+        assertEquals(dbAssignment.getEarnedPoints(), new Integer(84));
+        assertEquals(dbAssignment.getAssignedDate(), "09-20-2020");
+        assertEquals(dbAssignment.getDueDate(), "09-20-2020");
+        assertEquals(dbAssignment.getCategoryName(), "Test");
+    }
+
+    @Test
+    public void testUpdateAssignment() throws Exception {
+        // insert course and assignment into database
+        Course course = new Course(1, "Drew Clickenbeard", "CST 438", "Intro to software engineering", "08-24-2020", "12-24-2020", 90.3);
+        db.courseDao().insertCourse(course);
+        Assignment assignment = new Assignment("Exam 1", "This is the first exam", 100, 84, "09-20-2020", "09-20-2020", "Test", course.getCourseID());
+        db.AssignmentDao().insertAssignment(assignment);
+
+        // retrieve the assignment from the database and check its validity
+        ArrayList<Assignment> allAssignments = (ArrayList<Assignment>) db.AssignmentDao().getAllAssignments();
+        Assignment dbAssignment = db.AssignmentDao().getAssignmentWithId(allAssignments.get(0).getAssignmentID());
+        assertEquals(dbAssignment.getName(), "Exam 1");
+
+        // update the data and check to make sure updates persisted
+        dbAssignment.setName("Software Engineering Exam");
+        dbAssignment.setEarnedPoints(88);
+        db.AssignmentDao().updateAssignment(dbAssignment);
+        assertEquals(dbAssignment.getName(), "Software Engineering Exam");
+        assertEquals(dbAssignment.getEarnedPoints(), new Integer(88));
+    }
+
+    @Test
+    public void testDeleteAssignment() throws Exception {
+        // insert course and assignment into database
+        Course course = new Course(1, "Drew Clickenbeard", "CST 438", "Intro to software engineering", "08-24-2020", "12-24-2020", 90.3);
+        db.courseDao().insertCourse(course);
+        Assignment assignment = new Assignment("Exam 1", "This is the first exam", 100, 84, "09-20-2020", "09-20-2020", "Test", course.getCourseID());
+        db.AssignmentDao().insertAssignment(assignment);
+
+        // retrieve the assignment from the database and check its validity
+        ArrayList<Assignment> allAssignments = (ArrayList<Assignment>) db.AssignmentDao().getAllAssignments();
+        Assignment dbAssignment = db.AssignmentDao().getAssignmentWithId(allAssignments.get(0).getAssignmentID());
+        assertEquals(dbAssignment.getName(), "Exam 1");
+
+        // delete assignment and check if assignment has been deleted
+        Integer assignmentId = dbAssignment.getAssignmentID();
+        db.AssignmentDao().deleteAssignment(dbAssignment);
+        assertTrue(db.AssignmentDao().getAssignmentWithId(assignmentId) ==  null);
+    }
+
+    @Test
+    public void testGetAllAssignments() throws Exception {
+        // insert course and assignments into database
+        Course course = new Course(1, "Drew Clickenbeard", "CST 438", "Intro to software engineering", "08-24-2020", "12-24-2020", 90.3);
+        db.courseDao().insertCourse(course);
+        Assignment assignment1 = new Assignment("Exam 1", "This is the first exam", 100, 84, "09-20-2020", "09-20-2020", "Test", course.getCourseID());
+        Assignment assignment2 = new Assignment("Exam 2", "This is the second exam", 100, 94, "10-20-2020", "10-20-2020", "Test", course.getCourseID());
+        db.AssignmentDao().insertAssignment(assignment1);
+        db.AssignmentDao().insertAssignment(assignment2);
+
+        // check if all assignments have been retrieved
+        ArrayList<Assignment> allAssignments = (ArrayList<Assignment>) db.AssignmentDao().getAllAssignments();
+        assertEquals(allAssignments.get(0).getName(), "Exam 1");
+        assertEquals(allAssignments.get(1).getName(), "Exam 2");
+    }
+
+    @Test
+    public void testDeleteAllAssignments() throws Exception {
+        // insert course and assignments into database
+        Course course = new Course(1, "Drew Clickenbeard", "CST 438", "Intro to software engineering", "08-24-2020", "12-24-2020", 90.3);
+        db.courseDao().insertCourse(course);
+        Assignment assignment1 = new Assignment("Exam 1", "This is the first exam", 100, 84, "09-20-2020", "09-20-2020", "Test", course.getCourseID());
+        Assignment assignment2 = new Assignment("Exam 2", "This is the second exam", 100, 94, "10-20-2020", "10-20-2020", "Test", course.getCourseID());
+        db.AssignmentDao().insertAssignment(assignment1);
+        db.AssignmentDao().insertAssignment(assignment2);
+
+        // check if all assignments have been retrieved
+        ArrayList<Assignment> allAssignments = (ArrayList<Assignment>) db.AssignmentDao().getAllAssignments();
+        assertEquals(allAssignments.get(0).getName(), "Exam 1");
+        assertEquals(allAssignments.get(1).getName(), "Exam 2");
+
+        // check if all assignments have been deleted
+        db.AssignmentDao().deleteAllAssignment();
+        assertEquals(db.AssignmentDao().getAllAssignments().size(), 0);
+    }
+}
+
--- app/src/main/java/com/example/gradetracker/DB/UserDao.java
+++ app/src/main/java/com/example/gradetracker/DB/UserDao.java
@@ -1,36 +1,65 @@
 package com.example.gradetracker.DB;
 
-import com.example.gradetracker.Model.User;
-
-import java.util.List;
-
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
+import com.example.gradetracker.Model.User;
+
+import java.util.List;
+
 @Dao
 public interface UserDao {
 
+    /**
+     * Insert an user into the database
+     * @param user
+     */
     @Insert
     void insertUser(User user);
 
+    /**
+     * Update attributes of an user
+     * @param user
+     */
     @Update
     void updateUser(User user);
 
+    /**
+     * Delete a user from the database
+     * @param user
+     */
     @Delete
     void deleteUser(User user);
 
+    /**
+     * Delete all users from the database
+     */
     @Query("DELETE FROM users")
     void deleteAllUsers();
 
+    /**
+     * Retrieve all users from the database
+     * @return a list of all the users in the database
+     */
     @Query("SELECT * FROM users")
     List<User> getAllUsers();
 
+    /**
+     * Retrieve the user with the specified username
+     * @param search
+     * @return the user with the specified username
+     */
     @Query("SELECT * FROM users where username LIKE :search")
     User getUserWithUsername(String search);
 
+    /**
+     * Retrieve the user with the specified user id
+     * @param userId
+     * @return the user with the specified user id
+     */
     @Query("SELECT username from users where userID=:userId")
     String getUserById(Integer userId);
 }
